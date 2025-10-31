9a9e87acfe535d2af191f259892499627932eece
Thoritie <thohirh9@gmail.com>
2025-10-31 21:37:11 +0700

    feat: update field name :snowman:

--- api/app/routers/words.py
+++ api/app/routers/words.py
@@ -8,12 +8,12 @@ router = APIRouter()
 
 
 words = [
-    { "word": "Ephemeral", "meaning": "Lasting for a very short time.", "difficulty": "Advanced" },
-    { "word": "Ubiquitous", "meaning": "Present, appearing, or found everywhere.", "difficulty": "Intermediate" },
-    { "word": "Mellifluous", "meaning": "(Of a voice or words) sweet or musical; pleasant to hear.", "difficulty": "Advanced" },
-    { "word": "Serendipity", "meaning": "The occurrence and development of events by chance in a happy or beneficial way.", "difficulty": "Intermediate" },
-    { "word": "Happy", "meaning": "Feeling or showing pleasure or contentment.", "difficulty": "Beginner" },
-    { "word": "Run", "meaning": "Move at a speed faster than a walk, never having both or all the feet on the ground at the same time.", "difficulty": "Beginner" }
+    { "word": "Ephemeral", "definition": "Lasting for a very short time.", "difficulty": "Advanced" },
+    { "word": "Ubiquitous", "definition": "Present, appearing, or found everywhere.", "difficulty": "Intermediate" },
+    { "word": "Mellifluous", "definition": "(Of a voice or words) sweet or musical; pleasant to hear.", "difficulty": "Advanced" },
+    { "word": "Serendipity", "definition": "The occurrence and development of events by chance in a happy or beneficial way.", "difficulty": "Intermediate" },
+    { "word": "Happy", "definition": "Feeling or showing pleasure or contentment.", "difficulty": "Beginner" },
+    { "word": "Run", "definition": "Move at a speed faster than a walk, never having both or all the feet on the ground at the same time.", "difficulty": "Beginner" }
 ]
 
 @router.get("/word", response_model=WordResponse)
